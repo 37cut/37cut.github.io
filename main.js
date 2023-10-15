ad9d686827dddf066064003998699094cd4b556d
37cut <148013972+37cut@users.noreply.github.com>
2023-10-16 06:07:44 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+function amountToIndex(e) {
+	// Array(index) starts from 0.
+	return e.length - 1;
+}
+
+// Selectors
+function selectAll(e) {
+	return document.querySelectorAll(e);
+}
+
+function selectById(e) {
+	return document.getElementById(e);
+}
+
+// t: type, e: elements, l: limit, s: step.
+// bg&fg: background&foreground(font color).
+// zae: Zero and even, o: odd.
+function colorScheme(t, e, l, s, bg, fg) {
+	if (t=='zae') {
+		for (let i = 0; i <= l; i += s) {
+			e[i].style.backgroundColor = bg;
+			e[i].style.color = fg;
+			// console.log("Zero+Even:",i);
+		}		
+	} else if (t=='o') {
+		for (let i = 1; i <= l; i += s) {
+			e[i].style.backgroundColor = bg;
+			e[i].style.color = fg;
+			// console.log("Odd:",i);
+		}
+	} else {
+		return 0;
+	}
+}
+
+// Select elements.
+const section = selectAll('#section');
+
+// Index.
+const sectionIndex = amountToIndex(section);
+
+// DEBUG
+// console.log(importantTagIndex)
+// console.log(sectionIndex)
+
+// Colorscheme
+if (sectionIndex != 0) {
+	colorScheme('zae', section, sectionIndex, 2, 'powderblue', 'steelblue');
+	colorScheme('o', section, sectionIndex, 2, 'darkseagreen', 'teal');
+} else {
+	colorScheme('zae', section, sectionIndex, 1, 'powderblue', 'steelblue');
+}
+
---
+++ upload.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Commit on $(date +'%Y-%m-%d %H:%M:%S')"
+git push -u origin master 
