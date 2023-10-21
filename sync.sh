837870b653216dd45f22ef6ffaaeb2a630b6f29c
37cut <cutt37@outlook.com>
2023-10-21 21:13:42 +0800

    Project submit on 2023-10-21 21:13:42

--- sync.sh
+++ sync.sh
@@ -2,4 +2,5 @@ git init
 git remote add origin git@github.com:37cut/37cut.github.io.git
 git add .
 git commit -m "Project submit on $(date +'%Y-%m-%d %H:%M:%S')"
+git fetch
 git branch --set-upstream-to=origin/main main
