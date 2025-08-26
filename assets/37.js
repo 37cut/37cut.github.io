583cad14d7ca95d6b5ae6ecedd54db3063d45925
37cut <148013972+37cut@users.noreply.github.com>
2025-08-26 16:59:11 +0800

    Add files via upload

--- assets/37.js
+++ assets/37.js
@@ -1,14 +1,28 @@
+
+class Element {
+    constructor(text, func) {
+        this.text = text
+        this.func = func
+    }
+
+    createInstance() {
+        const div = document.createElement('div')
+        const p = document.createElement('p')
+
+        div.setAttribute('class', '_button')
+
+        div.onclick = this.func
+
+        p.appendChild(document.createTextNode(this.text))
+        div.appendChild(p)
+        footer.appendChild(div)
+    }
+}
+
 const footer = document.createElement('div')
-const profile = document.createElement('div')
-const profileText = document.createElement('p')
-
 footer.setAttribute('id', 'footer')
-profile.setAttribute('class', '_button')
 
-profile.onclick = function() { window.location.href = "https://osu.ppy.sh/users/17589673" }
-
-profileText.appendChild(document.createTextNode('Return'))
-profile.appendChild(profileText)
-footer.appendChild(profile)
+const Profile = new Element('Return.', function() { window.location.href = "https://osu.ppy.sh/users/17589673" })
+Profile.createInstance()
 
 document.body.appendChild(footer)
--- assets/main.js
+++ assets/main.js
@@ -1,31 +1,38 @@
+class Element {
+    constructor(text, func) {
+        this.text = text
+        this.func = func
+    }
+
+    createInstance() {
+        const div = document.createElement('div')
+        const p = document.createElement('p')
+
+        div.setAttribute('class', '_button')
+
+        div.onclick = this.func
+
+        p.appendChild(document.createTextNode(this.text))
+        div.appendChild(p)
+        footer.appendChild(div)
+    }
+}
+
 const scroll_height = document.documentElement.scrollHeight
 const inner_height = window.innerHeight
 
 const footer = document.createElement('div')
-const return_home = document.createElement('div')
-const return_homeContent = document.createElement('p')
-
 footer.setAttribute('id', 'footer')
 
-return_home.setAttribute('class', '_button')
-return_homeContent.appendChild(document.createTextNode('Return home.'))
-return_home.appendChild(return_homeContent)
-return_home.onclick = function() {window.location='../../'}
+const Home = new Element('Home.', function() { window.location = '../../' })
+Home.createInstance()
 
 if (scroll_height > innerHeight) {
-    const backto_top = document.createElement('div')
-    const backto_topContent = document.createElement('p')
-
-    backto_top.setAttribute('class', '_button')
-    backto_topContent.appendChild(document.createTextNode('Back to top.'))
-    backto_top.appendChild(backto_topContent)
-    footer.appendChild(backto_top)
-
-    backto_top.onclick = function() {
+    const Top = new Element('Back to Top.', function() {
         window.scrollTo({
             top: 0, left: 0,
-            behavior: 'smooth'})}
+            behavior: 'smooth'}) })
+    Top.createInstance()
 }
 
-footer.appendChild(return_home)
 document.body.appendChild(footer)
