ffed10e0b0f6775d018133c0f09098c65c567f5b
37cut <148013972+37cut@users.noreply.github.com>
2026-08-26 21:20:56 +0800

    Add files via upload

---
+++ web_assets/main.js
@@ -0,0 +1,62 @@
+function init() {
+    const img = document.createElement('img');
+    const imb = document.getElementById('imb');
+    const hover = document.getElementById('hover');
+    
+    const mobile = /Mobile|Android|iPhone/i.test(navigator.userAgent);
+    
+    img.src = 'https://cutt37.is-a.dev/web_assets/loli';
+    img.id = 'fixed-image';
+
+    img.style.position = 'fixed';
+    img.style.pointerEvents = 'none';
+
+    img.style.right = '8px';
+    img.style.bottom = '8px';
+
+    img.style.height = '72vh';
+    img.style.opacity = '0';
+    img.style.transition = 'opacity 0.5s';
+
+    if (!mobile) {
+        document.body.style.maxWidth = '1024px';
+        document.body.style.marginInline = 'auto';
+        document.body.style.paddingInline = '8px';
+        
+        hover.addEventListener('mouseover', function() {
+            img.style.opacity = '0.6';
+            img.style.right = window.getComputedStyle(document.body).marginRight;
+
+            hover.style.fontWeight = 'bold';
+        })
+
+        hover.addEventListener('mouseout', function() {
+            img.style.opacity = '0';
+            img.style.right = window.getComputedStyle(document.body).marginRight;
+
+            hover.style.fontWeight = 'normal';
+        })
+
+    }
+    
+    else {
+        mui_support();
+
+        hover.addEventListener('touchstart', function() {
+            img.style.opacity = '0.6';
+            here.style.fontWeight = 'bold';
+        })
+
+        hover.addEventListener('touchend', function() {
+            img.style.opacity = '0';
+            hover.style.fontWeight = 'normal';
+        })
+
+    }
+
+    document.body.appendChild(img);
+    document.body.style.display = 'block';
+    document.body.style.height = '95vh';
+}
+
+window.addEventListener('DOMContentLoaded', init)
